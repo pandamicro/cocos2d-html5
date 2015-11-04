50087a8028bb48b071e81ba9b3dad4fec8e8cae4
Huabing.Xu <dabingnn@gmail.com>
2015-11-04 10:28:38 +0800

    raw version of scale 9

--- extensions/ccui/base-classes/UIScale9SpriteWebGLRenderCmd.js
+++ extensions/ccui/base-classes/UIScale9SpriteWebGLRenderCmd.js
@@ -27,81 +27,106 @@
         return;
     ccui.Scale9Sprite.WebGLRenderCmd = function (renderable) {
         cc.Node.WebGLRenderCmd.call(this, renderable);
+        this._needDraw = true;
         this._cachedParent = null;
         this._cacheDirty = false;
+        this._quadWebBuffer = cc._renderContext.createBuffer();
     };
 
     var proto = ccui.Scale9Sprite.WebGLRenderCmd.prototype = Object.create(cc.Node.WebGLRenderCmd.prototype);
     proto.constructor = ccui.Scale9Sprite.WebGLRenderCmd;
 
-    proto.visit = function(parentCmd) {
+    proto.rendering = function (ctx){
         var node = this._node;
-        if(!node._visible)
-            return;
-        if(!node._scale9Image)
+        if(!node._scale9Enabled)
             return;
 
-        if (node._positionsAreDirty) {
-            node._updatePositions();
-            node._positionsAreDirty = false;
-            node._scale9Dirty = true;
+        var node = this._node, locTexture = null;
+        if(node.getSprite()) locTexture = node.getSprite()._texture;
+        if (!node.textureLoaded() || this._displayedOpacity === 0)
+            return;
+        var needRebuildWebBuffer = false;
+        if(node._quadsDirty){
+            node._cleanupSlicedSprites();
+            node._createSlicedSprites();
+            node._quadsDirty = false;
+            needRebuildWebBuffer = true;
         }
+        var gl = ctx || cc._renderContext ;
 
-        parentCmd = parentCmd || this.getParentRenderCmd();
-        if (node._parent && node._parent._renderCmd)
-            this._curLevel = node._parent._renderCmd._curLevel + 1;
+        if(locTexture != null)
+        {
+            this._shaderProgram.use();
+            this._shaderProgram._setUniformForMVPMatrixWithMat4(this._stackMatrix);
 
-        this._syncStatus(parentCmd);
+            cc.glBlendFunc(node._blendFunc.src, node._blendFunc.dst);
+            //optimize performance for javascript
+            cc.glBindTexture2DN(0, locTexture);                   // = cc.glBindTexture2D(locTexture);
+            cc.glEnableVertexAttribs(cc.VERTEX_ATTRIB_FLAG_POS_COLOR_TEX);
 
-        if(node._scale9Enabled) {
-            var locRenderers = node._renderers;
-            var rendererLen = locRenderers.length;
-            for(var j=0; j < rendererLen; j++) {
-                var renderer = locRenderers[j];
-                if(renderer) {
-                    var tempCmd = renderer._renderCmd;
-                    tempCmd.visit(this);
+            gl.bindBuffer(gl.ARRAY_BUFFER, this._quadWebBuffer);
+
+            var quads = node._quads;
+            var bufferOffset = 0;
+            var quadsLength = quads.length;
+            if(quadsLength == 0) return;
+            if (needRebuildWebBuffer)
+            {
+                gl.bufferData(gl.ARRAY_BUFFER,quads[0].arrayBuffer.byteLength * quads.length, gl.DYNAMIC_DRAW);
+                for(var i = 0; i < quads.length; ++i){
+                    gl.bufferSubData(gl.ARRAY_BUFFER, bufferOffset,quads[i].arrayBuffer);
+                    bufferOffset = bufferOffset + quads[i].arrayBuffer.byteLength;
                 }
-                else
-                    break;
             }
+            bufferOffset = 0;
+            for(var i = 0; i < quads.length; ++i)
+            {
+                gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 24, 0 + bufferOffset);                   //cc.VERTEX_ATTRIB_POSITION
+                gl.vertexAttribPointer(1, 4, gl.UNSIGNED_BYTE, true, 24, 12 + bufferOffset);           //cc.VERTEX_ATTRIB_COLOR
+                gl.vertexAttribPointer(2, 2, gl.FLOAT, false, 24, 16 + bufferOffset);                  //cc.VERTEX_ATTRIB_TEX_COORDS
+                gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+                bufferOffset = bufferOffset + quads[i].arrayBuffer.byteLength;
+            }
+
+            cc.g_NumberOfDraws += quads.length;
+
         }
-        else {
-            node._adjustScale9ImageScale();
-            node._adjustScale9ImagePosition();
-            node._scale9Image._renderCmd.visit(this);
-        }
-        this._dirtyFlag = 0;
-        cc.Node.WebGLRenderCmd.prototype.visit.call(this, parentCmd);
+
+        //do scale9 draw
+        //{
+        //    cc.log("I am doing the real scale9 draw");
+        //}
     };
 
     proto.transform = function(parentCmd, recursive){
         var node = this._node;
         cc.Node.WebGLRenderCmd.prototype.transform.call(this, parentCmd, recursive);
-        if (node._positionsAreDirty) {
-            node._updatePositions();
-            node._positionsAreDirty = false;
-            node._scale9Dirty = true;
-        }
-        if(node._scale9Enabled) {
-            var locRenderers = node._renderers;
-            var protectChildLen = locRenderers.length;
-            for(var j=0; j < protectChildLen; j++) {
-                var pchild = locRenderers[j];
-                if(pchild) {
-                    var tempCmd = pchild._renderCmd;
-                    tempCmd.transform(this, true);
-                }
-                else
-                    break;
-            }
-        }
-        else {
-            node._adjustScale9ImageScale();
-            node._adjustScale9ImagePosition();
-            node._scale9Image._renderCmd.transform(this, true);
-        }
-
+        //if (node._positionsAreDirty) {
+        //    node._updatePositions();
+        //    node._positionsAreDirty = false;
+        //    node._scale9Dirty = true;
+        //}
+        //if(node._scale9Enabled) {
+        //    var locRenderers = node._renderers;
+        //    var protectChildLen = locRenderers.length;
+        //    for(var j=0; j < protectChildLen; j++) {
+        //        var pchild = locRenderers[j];
+        //        if(pchild) {
+        //            var tempCmd = pchild._renderCmd;
+        //            tempCmd.transform(this, true);
+        //        }
+        //        else
+        //            break;
+        //    }
+        //}
+        //else {
+        //    node._adjustScale9ImagePosition();
+        //    node._scale9Image._renderCmd.transform(this, true);
+        //}
+        //if(node._scale9Image)
+        //{
+        //    node._scale9Image._renderCmd.transform(this, true);
+        //}
     };
 
     proto._syncStatus = function (parentCmd){
@@ -114,42 +139,98 @@
         cc.Node.WebGLRenderCmd.prototype._updateDisplayColor.call(this, parentColor);
         var node = this._node;
         var scale9Image = node._scale9Image;
-        parentColor = this._displayedColor;
-        if(node._scale9Enabled) {
-            var pChildren = node._renderers;
-            for(var i=0; i<pChildren.length; i++) {
-                pChildren[i]._renderCmd._updateDisplayColor(parentColor);
-                pChildren[i]._renderCmd._updateColor();
+
+
+
+        if (this._cascadeColorEnabled)
+        {
+            node._displayedColor.r = node._realColor.r * parentColor.r/255.0;
+            node._displayedColor.g = node._realColor.g * parentColor.g/255.0;
+            node._displayedColor.b = node._realColor.b * parentColor.b/255.0;
+            this._updateColor();
+            var childrenSize= node._children.length;
+            for(var i = 0; i< childrenSize; ++i)
+            {
+                node._children[i]._renderCmd._updateDisplayColor(node._displayedColor);
+            }
+
+            if (scale9Image)
+            {
+                scale9Image._renderCmd._updateDisplayColor(node._displayedColor);
+            }
+
+        }
+        else
+        {
+            var childrenSize= node._children.length;
+            for(var i = 0; i< childrenSize; ++i)
+            {
+                node._children[i]._renderCmd._updateDisplayColor(cc.Color.WHITE);
+            }
+
+            if (scale9Image)
+            {
+                scale9Image._renderCmd._updateDisplayColor(cc.Color.WHITE);
             }
         }
-        else {
-            scale9Image._renderCmd._updateDisplayColor(parentColor);
-            scale9Image._renderCmd._updateColor();
-        }
+
     };
 
     proto._updateDisplayOpacity = function(parentOpacity){
         cc.Node.WebGLRenderCmd.prototype._updateDisplayOpacity.call(this, parentOpacity);
         var node = this._node;
         var scale9Image = node._scale9Image;
-        parentOpacity = this._displayedOpacity;
-        if(node._scale9Enabled) {
-            var pChildren = node._renderers;
-            for(var i=0; i<pChildren.length; i++)
-                pChildren[i]._renderCmd._updateDisplayOpacity(parentOpacity);
+
+        if (node._cascadeOpacityEnabled)
+        {
+            var childrenSize= node._children.length;
+            for(var i = 0; i< childrenSize; ++i)
+            {
+                node._children[i]._renderCmd._updateDisplayOpacity(node._displayedOpacity);
+            }
+            node._displayedOpacity = node._realOpacity * parentOpacity/255.0;
+            this._updateColor();
+
+            if (scale9Image != null)
+            {
+                scale9Image._renderCmd._updateDisplayOpacity(node._displayedOpacity);
+            }
         }
         else
-            scale9Image._renderCmd._updateDisplayOpacity(parentOpacity);
+        {
+            node._displayedOpacity = node._realOpacity;
+
+            var childrenSize= node._children.length;
+            for(var i = 0; i< childrenSize; ++i)
+            {
+                this._children[i]._renderCmd._updateDisplayOpacity(255);
+            }
+
+            if (scale9Image != null)
+            {
+                scale9Image._renderCmd._updateDisplayOpacity(255);
+            }
+        }
     };
 
     proto.setState = function (state) {
+        var node = this._node;
         var scale9Image = this._node._scale9Image;
-        if(scale9Image == null)
-            return;
-        if (state === ccui.Scale9Sprite.state.NORMAL) {
-            scale9Image.setShaderProgram(cc.shaderCache.programForKey(cc.SHADER_POSITION_TEXTURECOLOR));
-        } else if (state === ccui.Scale9Sprite.state.GRAY) {
-            scale9Image.setShaderProgram(ccui.Scale9Sprite.WebGLRenderCmd._getGrayShaderProgram());
+        if(!node.isScale9Enabled)
+        {
+            if (state === ccui.Scale9Sprite.state.NORMAL) {
+                scale9Image.setShaderProgram(cc.shaderCache.programForKey(cc.SHADER_POSITION_TEXTURECOLOR));
+            } else if (state === ccui.Scale9Sprite.state.GRAY) {
+                scale9Image.setShaderProgram(ccui.Scale9Sprite.WebGLRenderCmd._getGrayShaderProgram());
+            }
+        }
+        else
+        {
+            if (state === ccui.Scale9Sprite.state.NORMAL) {
+                node.setShaderProgram(cc.shaderCache.programForKey(cc.SHADER_POSITION_TEXTURECOLOR));
+            } else if (state === ccui.Scale9Sprite.state.GRAY) {
+                node.setShaderProgram(ccui.Scale9Sprite.WebGLRenderCmd._getGrayShaderProgram());
+            }
         }
     };
 
